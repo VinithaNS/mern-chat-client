5ff19d90d2c4bbf72a11b24e96ef4fb0c94a5148
vinithaneyashree <vinithaneyashree2901@gmail.com>
2022-12-12 21:24:53 +0530

    final commit

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
 import React from 'react'
 
-const SOCKET_URL = "https://mern-chat-server.onrender.com/";
+const SOCKET_URL = "https://mern-chat-server.onrender.com";
 
 export const socket = io(SOCKET_URL);
 
--- src/services/appApi.js
+++ src/services/appApi.js
@@ -1,38 +1,38 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react/'
 
 const appApi = createApi({
-    reducerPath: 'appApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://chat-app-serv.herokuapp.com"
+  reducerPath: "appApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://mern-chat-server.onrender.com",
+  }),
+  endpoints: (builder) => ({
+    //register
+    signupUser: builder.mutation({
+      query: (user) => ({
+        url: "/users",
+        method: "POST",
+        body: user,
+      }),
     }),
-    endpoints: (builder) => ({
-        //register
-        signupUser: builder.mutation({
-            query: (user) => ({
-                url: '/users',
-                method: 'POST',
-                body: user
-            })
-        }),
 
-        //login
-        loginUser: builder.mutation({
-            query: (user) => ({
-                url: '/users/login',
-                method: 'POST',
-                body: user
-            })
-        }),
+    //login
+    loginUser: builder.mutation({
+      query: (user) => ({
+        url: "/users/login",
+        method: "POST",
+        body: user,
+      }),
+    }),
 
-        //logout
-        logoutUser: builder.mutation({
-            query: (payload) => ({
-                url: "/logout",
-                method: "DELETE",
-                body: payload,
-            }),
-        }),
-    })
+    //logout
+    logoutUser: builder.mutation({
+      query: (payload) => ({
+        url: "/logout",
+        method: "DELETE",
+        body: payload,
+      }),
+    }),
+  }),
 });
 
 export const {useSignupUserMutation, useLoginUserMutation, useLogoutUserMutation} = appApi;
